4d0d495d4c5e318c8f038287c930158f8b090f85
Womika khan <76390845+womikakhan@users.noreply.github.com>
2022-12-29 09:36:53 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,18 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+static long num_steps = 100;
+double step;
+void main()
+{
+	int i; double x, pi, sum = 0.0;
+	step = 1.0 / (double)num_steps;
+#pragma omp parallel for private(x) reduction(+:sum)
+	for (i = 1; i <= num_steps; i++) 
+  {
+		x = (i - 0.5) * step;
+		sum = sum + 4.0 / (1.0 + x * x);
+	}
+	pi = sum / num_steps;
+	printf("pi = %6.8f\n", pi);
+}
