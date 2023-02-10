6be035d6a4926ba898a235a02809d39ee98f4ae7
Womika khan <76390845+womikakhan@users.noreply.github.com>
2023-02-10 15:31:59 +0530

    Update 7.c

--- 7.c
+++ 7.c
@@ -1,24 +1,42 @@
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
+#include <malloc.h>
 int main(int argc, char* argv[])
 {
-	int size, rank;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	float recvbuf, sendbuf[100];
-	if (rank == 0) {
-		int i;
-		printf("Before Scatter : sendbuf of rank 0 : ");
-		for (i = 0; i < size; i++) {
-			srand(i);
-			sendbuf[i] = (float)(rand()%1000)/10;
-			printf("%.1f ", sendbuf[i]);
-		}
-		printf("\nAfter Scatter :\n");
-	}
-	MPI_Scatter(sendbuf, 1, MPI_FLOAT, &recvbuf, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
-	printf("rank= %d Recvbuf: %.1f\n", rank, recvbuf);
-	MPI_Finalize();
+    MPI_Init(&argc, &argv);
+    int rank, q;
+    int root = 0;
+    int sarray[4];
+    int rarray[4];
+    int marray[4];
+    for (int i = 0; i < 4; i++)
+    {
+        sarray[i] = 0;
+        rarray[i] = 0;
+        marray[i] = 0;
+    }
+    q = 4;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0)
+    {
+        for (int i = 0; i < 4; i++) sarray[i] = 200 + i * 10;
+    }
+
+    if (rank < q)
+    {
+
+        MPI_Scatter(sarray, 1, MPI_INT, rarray, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+    printf(" Process [%d ] Send array %d %d %d %d \n", rank, sarray[0], sarray[1], sarray[2], sarray[3]);
+    printf(" Process [%d ] Receive array %d %d %d %d \n", rank, rarray[0], rarray[1], rarray[2], rarray[3]);
+    marray[0] = rarray[0] + rank * 2;
+    printf(" Process [%d ] Modified array %d %d %d %d \n", rank, marray[0], marray[1], marray[2], marray[3]);
+    MPI_Gather(marray, 1, MPI_INT, sarray, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Finalize();
+    if (rank == 0)
+    {
+        printf(" Process [%d ] Gathered array %d %d %d %d \n", 0, sarray[0], sarray[1], sarray[2], sarray[3]);
+    }
+    return 0;
 }
